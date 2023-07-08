e2ad459816be29725f4c2b95137b72af2fc9604f
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-07-08 21:35:57 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+// mBMR6LXFceQLi21H
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", true);
+const DB_HOST =
+  "mongodb+srv://Olga:mBMR6LXFceQLi21H@cluster0.asa0fxs.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000), console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
